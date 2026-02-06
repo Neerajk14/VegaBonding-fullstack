2cef71b373152752a9ce63286ea55bd7bba9d72e
Neeraj <neerajpatel2310@gmail.com>
2026-02-06 14:51:24 +0530

    Fixed default route problem

--- app.js
+++ app.js
@@ -88,7 +88,9 @@ app.use((req, res, next) => {
 app.use("/listings", listingsRouter);
 app.use("/listings/:id/reviews", reviewsRouter);
 app.use("/", userRouter);
-  
+  app.get("",(req,res)=>{
+    res.redirect("/listings")
+  })
 app.all(/(.*)/, (req, res, next) => {
   next(new ExpressError(404, "Page Not Found"));
 });
