527ec1847237857f94f67b2bd119b0a2d068510f
Neeraj <neerajpatel2310@gmail.com>
2026-03-21 09:15:21 +0530

    updated user  model

--- models/user.js
+++ models/user.js
@@ -1,14 +1,26 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const passportLocalMongoose =require("passport-local-mongoose").default;
+const passportLocalMongoose = require("passport-local-mongoose").default;
 
-const userSchema =new Schema({
-    email:{
-        type:String,
-        required:true
-    }
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  wishlist: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Listing",
+    },
+  ],
+  bookings: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Booking",
+    },
+  ],
 });
 
 userSchema.plugin(passportLocalMongoose);
 
-module.exports = mongoose.model("User",userSchema);
+module.exports = mongoose.model("User", userSchema);
